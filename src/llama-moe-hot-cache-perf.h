1d374073c6bad1f86235d84472865ac8a1d65530
adrianhoehne <adrian.alexander.hoehne@gmail.com>
2026-05-13 21:20:07 +0200

    Separate moe hot cache feature pt3.

---
+++ src/llama-moe-hot-cache-perf.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ggml-backend.h"
+
+#include <cstdint>
+
+struct ggml_tensor;
+struct llama_context;
+
+bool llama_moe_layer_perf_is_enabled(const llama_context * ctx);
+bool llama_moe_layer_perf_eval_callback(ggml_tensor * t, bool ask, void * user_data);
+
+void llama_moe_layer_perf_begin(uint32_t n_layer, uint32_t n_expert, uint32_t n_expert_used);
+void llama_moe_layer_perf_end();
+void llama_moe_layer_perf_collect_parallel_metrics(ggml_backend_sched_t sched);
